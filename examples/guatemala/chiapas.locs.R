413251a1a3fadab749e072e91bcd8577b2847c87
Mike Sandiford <msandifo@gmail.com>
2019-02-11 14:59:03 +1100

    second

--- examples/guatemala/chiapas.locs.R
+++
@@ -1,8 +0,0 @@
-
-
-get_slab_details(mex$slab ,longlat = c(-94.11, 14.85 ),plates= bird.plates.psp)
-
-cmt1.ppp$marks %>% subset(m>7.5 & lubridate::year(date)==2017) %>% dplyr::select(c(long, lat))
-isc.ppp$marks %>% subset(m>7.5 & lubridate::year(date)==2017) %>% dplyr::select(c(long, lat))
-
-get_slab_details(mex$slab, longlat=c(-93.71, 15.07 ) ,plates= bird.plates.psp)
